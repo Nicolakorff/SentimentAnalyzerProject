2513ed11bedf6f14f3b58f416e13a01daf66868a
Nicola Korff <nicolavonkorff@gmail.com>
2025-04-21 22:39:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detector")
+app = Flask(__name__) 
 
 @app.route("/emotionDetector")
 def emo_detector() :
-    """
-    It takes input text from user and send to appropriate function,
-    then return response based on user text emotions.
-    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     dominant_emotion = response['dominant_emotion']
@@ -19,11 +15,8 @@ def emo_detector() :
 
 @app.route("/")
 def render_index_page() :
-    """
-    It starts server on defined server and port for development.
-    """
     return render_template('index.html')
 
 if __name__ == "__main__" :
-    app.run(host = "0.0.0.0", port = 5000)
-    
+    app.run(host = "0.0.0.0", port = 5000, debug=True)
+ 
